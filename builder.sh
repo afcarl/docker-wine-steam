aad4182844eb4c82b6e494176b39c1d25339194b
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2018-03-05 15:27:02 -0800

    This is my commit. There are many like it, but this one is mine.

--- builder.sh
+++ builder.sh
@@ -3,14 +3,16 @@
 # Getting the user id.
 MYUID=`id -u`
 
+TEMP=$(mktemp -d)
+
 # Copying the Dockerfile to set parameters.
-cp -f Dockerfile /tmp/ && cp -f finalize_installation.sh /tmp/
+cp -f Dockerfile $TEMP/ && cp -f finalize_installation.sh $TEMP/
 
 # Setting the right user id.
-sed -i -e "s/1001/$MYUID/g" /tmp/Dockerfile
+sed -i -e "s/1001/$MYUID/g" $TEMP/Dockerfile
 
 # Building the image.
-sudo docker build -t webanck/docker-wine-steam /tmp/ 
+docker build -t webanck/docker-wine-steam $TEMP/
 
 # Cleaning up.
-rm /tmp/Dockerfile /tmp/finalize_installation.sh
+rm -rf $TEMP/
--- launcher.sh
+++ launcher.sh
@@ -4,18 +4,19 @@ CONTAINER_NAME=vaporized_wine
 
 ( \
 	echo 'Trying to run a new data container.' && \
-	sudo docker run -ti \
+	docker run -ti --privileged \
 			-e DISPLAY \
 			-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 			-v ~/.Xauthority:/home/wine/.Xauthority \
-			-v /dev/snd:/dev/snd --privileged \
+			-v /dev/snd:/dev/snd \
+                        -v /dev/input:/dev/input \
 			-v /run/user/`id -u`/pulse/native:/run/user/`id -u`/pulse/native \
 			-v `pwd`/shared_directory:/home/wine/shared_directory \
 			--net=host \
 			--name "$CONTAINER_NAME" \
-			webanck/docker-wine-steam \
+			winesteam \
 	2>/dev/null \
 ) || ( \
 	echo 'The container already exists, relaunching the old old one.' && \
-	sudo docker start -ai "$CONTAINER_NAME" \
+	docker start -ai "$CONTAINER_NAME" \
 )
